f56ccdb6024ca2d3ab0e5756c258c2fe2cb6d96e
Tom Magin <magintc@mac.com>
2019-05-13 15:20:28 +1000

    fixed warning

--- src/components/AllPost.js
+++ src/components/AllPost.js
@@ -5,9 +5,6 @@ import EditComponent from "./EditComponent";
 import "../css/AllPost.css";
 import PropTypes from "prop-types";
 class AllPost extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div id="h1">
