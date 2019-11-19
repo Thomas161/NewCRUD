6705361268edfbb26d4ca2f8672e0514f8807cb4
Tom Magin <magintc@mac.com>
2019-11-19 15:48:01 +1100

    finished with this project

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,65 +2,51 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { EDIT_POST, DELETE_POST } from "../actions";
-import { CSSTransition } from "react-transition-group";
 import s from "../css/Post.module.css";
-import "../css/style.css";
 
 class Post extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: false
-    };
-  }
   render() {
     const {
-      post: { title, message }
+      post: { title, message },
+      head
     } = this.props;
-    const { head } = this.props;
     return (
       <div className={s.pos}>
         <div>
           <h1 className={s.h1}>{head}</h1>
         </div>
-        <CSSTransition
-          in={!this.state.display}
-          timeout={350}
-          classNames="list"
-          unmountOnExit
-        >
-          <div className="ui card">
-            <div className="content">
-              <h3 className={s.h3}>{title}</h3>
-              <h4 className={s.h4}>{message}</h4>
 
-              <button
-                onClick={() =>
-                  this.props.dispatch({
-                    type: EDIT_POST,
-                    id: this.props.post.id
-                  })
-                }
-                className="circular ui icon button green"
-              >
-                <i className="icon edit"></i>
-                Edit
-              </button>
-              <button
-                onClick={() =>
-                  this.props.dispatch({
-                    type: DELETE_POST,
-                    id: this.props.post.id
-                  })
-                }
-                className="circular ui icon button red"
-              >
-                <i className="icon trash"></i>
-                Delete
-              </button>
-            </div>
+        <div className="ui card transition">
+          <div className="content">
+            <h3 className={s.h3}>{title}</h3>
+            <h4 className={s.h4}>{message}</h4>
+
+            <button
+              onClick={() =>
+                this.props.dispatch({
+                  type: EDIT_POST,
+                  id: this.props.post.id
+                })
+              }
+              className="circular ui icon button green"
+            >
+              <i className="icon edit"></i>
+              Edit
+            </button>
+            <button
+              onClick={() =>
+                this.props.dispatch({
+                  type: DELETE_POST,
+                  id: this.props.post.id
+                })
+              }
+              className="circular ui icon button red"
+            >
+              <i className="icon trash"></i>
+              Delete
+            </button>
           </div>
-        </CSSTransition>
+        </div>
       </div>
     );
   }
--- src/reducers/postreducer.js
+++ src/reducers/postreducer.js
@@ -1,12 +1,19 @@
+// import _ from "lodash";
 import { ADD_POST, DELETE_POST, EDIT_POST, UPDATE_POST } from "../actions";
 
-export const postReducer = (state = [], action) => {
+const initState = [];
+export const postReducer = (state = initState, action) => {
   const { type } = action;
   switch (type) {
     case ADD_POST:
       return state.concat([action.data]);
     case DELETE_POST:
       const newState = state.filter(post => post.id !== action.id);
+      // const newState = _.mapKeys(state, action.id);
+      // console.log(newState);
+      // //return _.omit(state.filter(post => post.id !== action.id));
+      // return _.omit(state,'newState');
+
       return newState;
     case EDIT_POST:
       return state.map(post =>
